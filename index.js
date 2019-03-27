2015b9bf20918802c1af52a1c7b2630532003de9
Colton G. Rushton <colton51919@gmail.com>
2019-03-26 22:22:26 -0300

    Update index.js
    Updates index.js to add every hashing format under the sun as of 11.12.0 of node and this comment

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ const create = algorithm => async (buffer, options) => {
 	return hash.digest().buffer;
 };
 
+exports.ripemd160 = create('ripemd160');
+exports.blake32s256 = create('blake32s256');
+exports.blake32b512 = create('blake32b512');
+exports.md4 = create('md4');
+exports.md5 = create('md5');
 exports.sha1 = create('sha1');
+exports.sha224 = create('sha224');
 exports.sha256 = create('sha256');
 exports.sha384 = create('sha384');
 exports.sha512 = create('sha512');
+exports.whirlpool = create('whirlpool');
