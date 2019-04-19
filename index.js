939595c67aa57500b05b367a16fcd117f4fbd089
Yanis Benson <yanis.benson@gmail.com>
2019-04-19 18:48:43 +0300

    refactoring

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const taskWorker = (value, transferList) => new Promise(resolve => {
 	tasks.set(id, resolve);
 	if (worker === undefined) {
 		worker = new Worker('./thread.js');
-		worker.on('message', msg => {
-			const task = tasks.get(msg.id);
-			tasks.delete(msg.id);
+		worker.on('message', message => {
+			const task = tasks.get(message.id);
+			tasks.delete(message.id);
 			if (tasks.size === 0) {
 				worker.unref();
 			}
 
-			task(msg.value);
+			task(message.value);
 		});
 		worker.on('error', err => {
 			// Any error here is effectively an equivalent of segfault, and have no scope, so we just throw it on callback level
