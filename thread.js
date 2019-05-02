655e0823348868ead980d02a6c9e3fb4ba06313d
Yanis Benson <yanis.benson@gmail.com>
2019-05-02 17:30:08 +0300

    argument naming

--- thread.js
+++ thread.js
@@ -2,11 +2,11 @@
 const {parentPort} = require('worker_threads');
 const crypto = require('crypto');
 
-parentPort.on('message', msg => {
-	const {algorithm, buffer} = msg.value;
+parentPort.on('message', message => {
+	const {algorithm, buffer} = message.value;
 	const hash = crypto.createHash(algorithm);
 	hash.update(Buffer.from(buffer));
-	const arrayBuf = hash.digest().buffer;
+	const arrayBuffer = hash.digest().buffer;
 	// Transfering buffer here for consistency, but considering buffer size it might be faster to just leave it for copying, needs perf test
-	parentPort.postMessage({id: msg.id, value: arrayBuf}, [arrayBuf]);
+	parentPort.postMessage({id: message.id, value: arrayBuffer}, [arrayBuffer]);
 });
