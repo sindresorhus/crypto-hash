d6a13c305ba3183d14fb7e58f88c6121898450ef
Yanis Benson <yanis.benson@gmail.com>
2019-05-03 08:34:43 +0300

    test linter again

--- thread.js
+++ thread.js
@@ -1,15 +1,6 @@
 'use strict';
 const crypto = require('crypto');
-
-const requireOptional = (name, defaultValue) => {
-	try {
-		return require(name);
-	} catch (error) {
-		return defaultValue;
-	}
-};
-
-const {parentPort} = requireOptional('worker_threads', {});
+const {parentPort} = require('worker_threads');
 
 parentPort.on('message', message => {
 	const {algorithm, buffer} = message.value;
@@ -17,5 +8,8 @@ parentPort.on('message', message => {
 	hash.update(Buffer.from(buffer));
 	const arrayBuffer = hash.digest().buffer;
 	// Transfering buffer here for consistency, but considering buffer size it might be faster to just leave it for copying, needs perf test
+	parentPort.postMessage({id: message.id, value: arrayBuffer}, [arrayBuffer]);
+});
+ight be faster to just leave it for copying, needs perf test
 	parentPort.postMessage({id: message.id, value: arrayBuffer}, [arrayBuffer]);
 });
