223353633e718cd129cd3108d3b2bdba2cb23c1a
Richie Bendall <richiebendall@gmail.com>
2022-06-23 05:04:54 +1200

    Minor tweak (#32)

--- index.js
+++ index.js
@@ -2,43 +2,6 @@ import {Buffer} from 'node:buffer';
 import {Worker} from 'node:worker_threads';
 import crypto from 'node:crypto';
 
-const threadFilePath = new URL('thread.js', import.meta.url);
-
-let worker; // Lazy
-let taskIdCounter = 0;
-const tasks = new Map();
-
-const createWorker = () => {
-	worker = new Worker(threadFilePath);
-
-	worker.on('message', message => {
-		const task = tasks.get(message.id);
-		tasks.delete(message.id);
-		if (tasks.size === 0) {
-			worker.unref();
-		}
-
-		task(message.value);
-	});
-
-	worker.on('error', error => {
-		// Any error here is effectively an equivalent of segfault and have no scope, so we just throw it on callback level
-		throw error;
-	});
-};
-
-const taskWorker = (value, transferList) => new Promise(resolve => {
-	const id = taskIdCounter++;
-	tasks.set(id, resolve);
-
-	if (worker === undefined) {
-		createWorker();
-	}
-
-	worker.ref();
-	worker.postMessage({id, value}, transferList);
-});
-
 let create = algorithm => async (buffer, {outputFormat = 'hex'} = {}) => {
 	const hash = crypto.createHash(algorithm);
 	hash.update(buffer, typeof buffer === 'string' ? 'utf8' : undefined);
@@ -51,6 +14,43 @@ let create = algorithm => async (buffer, {outputFormat = 'hex'} = {}) => {
 };
 
 if (Worker !== undefined) {
+	const threadFilePath = new URL('thread.js', import.meta.url);
+
+	let worker; // Lazy
+	let taskIdCounter = 0;
+	const tasks = new Map();
+
+	const createWorker = () => {
+		worker = new Worker(threadFilePath);
+
+		worker.on('message', message => {
+			const task = tasks.get(message.id);
+			tasks.delete(message.id);
+			if (tasks.size === 0) {
+				worker.unref();
+			}
+
+			task(message.value);
+		});
+
+		worker.on('error', error => {
+			// Any error here is effectively an equivalent of segfault and have no scope, so we just throw it on callback level
+			throw error;
+		});
+	};
+
+	const taskWorker = (value, transferList) => new Promise(resolve => {
+		const id = taskIdCounter++;
+		tasks.set(id, resolve);
+
+		if (worker === undefined) {
+			createWorker();
+		}
+
+		worker.ref();
+		worker.postMessage({id, value}, transferList);
+	});
+
 	create = algorithm => async (source, {outputFormat = 'hex'} = {}) => {
 		let buffer;
 		if (typeof source === 'string') {
